87bfef2814a37140c71199151b2bf639df95fd60
konstPialov <azure_dream@mail.ru>
2021-05-05 15:56:10 +0300

    os.environ

--- data/config.py
+++ data/config.py
@@ -5,14 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
+# BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
+BOT_TOKEN = str(os.environ['BOT_TOKEN'])
 
 print('BOT_TOKEN: ', BOT_TOKEN)
 
 
 ###
 ###
-# from dotenv import dotenv_values
+# from .env import dotenv_values
 
 # env_data = dotenv_values('.env')
 # print(env_data['BOT_TOKEN'])
